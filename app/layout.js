0dcc3e7216307d93c838d2b540f7946db533dd80
DerekCFI <DEREKMATTHEWSW@GMAIL.COM>
2026-01-09 19:47:18 +0000

    Fix duplicate navigation links and reorder menu items

--- app/layout.js
+++ app/layout.js
@@ -16,44 +16,25 @@ export default function RootLayout({ children }) {
             <Link href="/" className="text-2xl font-bold">
               CrewBase
             </Link>
-            <div className="flex gap-6">
+            <div className="flex items-center gap-6">
               <Link href="/hotels" className="hover:underline">
                 Hotels
               </Link>
               <Link href="/fbos" className="hover:underline">
                 FBOs
               </Link>
-              <Link href="/restaurants" className="hover:underline">
-                Restaurants
-              </Link>
               <Link href="/rentals" className="hover:underline">
                 Car Rentals
+              </Link>
+              <Link href="/restaurants" className="hover:underline">
+                Restaurants
               </Link>
 
               <Link href="/add" className="bg-white text-blue-600 px-4 py-2 rounded font-semibold hover:bg-gray-100">
                 + Add Location
               </Link>
-              <div className="flex items-center gap-6">
-  <Link href="/hotels" className="hover:underline">
-    Hotels
-  </Link>
-  <Link href="/fbos" className="hover:underline">
-    FBOs
-  </Link>
-  <Link href="/restaurants" className="hover:underline">
-    Restaurants
-  </Link>
-  <Link href="/rentals" className="hover:underline">
-    Car Rentals
-  </Link>
 
-  <Link href="/add" className="bg-white text-blue-600 px-4 py-2 rounded font-semibold hover:bg-gray-100">
-    + Add Location
-  </Link>
-
-  <HeaderSearch />
-</div>
-
+              <HeaderSearch />
             </div>
           </div>
         </nav>
